9be2c2c0a14875c6d0c14d3b6a71028507b9406d
Karan Kaushik <kkricardokaka95@gmail.com>
2020-04-13 16:40:17 +1000

    Rescue Boats

---
+++ RescueBoats.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int numRescueBoats(int[] people, int limit) {
+        int noOfBoats = 0;
+        
+        // Sort the array
+        Arrays.sort(people);
+        
+        int start = 0;
+        int end = people.length-1;
+        
+        while(start<=end){
+            noOfBoats++; // Heaviest always gets a boat
+            
+            // If lightest has space to fit in it, accommodate him and move the pointer
+            if(people[start]+people[end]<=limit)
+                start++;
+            
+            end--;
+        }
+        
+        return noOfBoats;
+    }
+}
