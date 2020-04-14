5e46b04cc46da8a0803c36b15b65866d3f6bfc82
Karan Kaushik <kkricardokaka95@gmail.com>
2020-04-15 00:54:07 +1000

    Stupid inefficient shares

---
+++ Shares.java
@@ -0,0 +1,29 @@
+import java.util.*;
+
+class Shares {
+    public static int maxProfit(int[] prices) {
+
+        int buyPrice = Integer.MAX_VALUE;
+        int profit = 0;
+        
+        for(int i =0; i<prices.length-1;i++){
+            if(prices[i]<buyPrice){
+                for(int j =i+1; j<prices.length;j++){
+                    if(prices[j] - prices[i] > profit){
+                        buyPrice = prices[i];
+                        profit = prices[j] - prices[i];
+                    }
+                }
+            }
+        }
+        
+        return profit;
+
+    }
+
+    public static void main(String[] args){
+        int[] prices = {2,1,4};
+        System.out.println(maxProfit(prices));
+
+    }
+}
