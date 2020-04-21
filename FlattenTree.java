8ec57aaa4b4326dbc5facd5d347b9b2a2f52483d
Karan Kaushik <kkricardokaka95@gmail.com>
2020-04-21 23:50:25 +1000

    Update FlattenTree.java

--- FlattenTree.java
+++ FlattenTree.java
@@ -1,3 +1,5 @@
+// https://leetcode.com/problems/flatten-binary-tree-to-linked-list
+
 import java.util.*;
 
 class TreeNode {
@@ -12,22 +14,31 @@ class TreeNode {
 
 class FlattenTree {
     public void flatten(TreeNode root) {
-        
+        // Base case of recursion, if reached leaf or tree is null
         if(root==null){
             return;
         }
         
+        // If there's a right node, recursively flatten it
         if(root.right != null){
             flatten(root.right);
         }
+        
+        // If the tree has a left
         if(root.left != null){
-            flatten(root.left);
+            flatten(root.left); // Flatten it recursively first
+            
+            // Go into its rightmost child
             TreeNode temp = root.left;
+            
             while(temp.right != null){
                 temp = temp.right;
-            }    
+            }
+            
+            // Attach it to the right child
             temp.right = root.right;
             root.right = root.left;
+            
             root.left = null;
         }
     }
