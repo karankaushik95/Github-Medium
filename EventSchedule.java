959c9975695ea940d3c3a8b3a6c0d3f2609a4eff
Karan Kaushik <kkricardokaka95@gmail.com>
2020-04-21 23:43:59 +1000

    EventSchedule

--- EventSchedule.java
+++ EventSchedule.java
@@ -1,3 +1,5 @@
+// https://leetcode.com/problems/maximum-number-of-events-that-can-be-attended/submissions/
+
 class EventSchedule {
     public int maxEvents(int[][] events) {
         int noOfEvents = 0;
